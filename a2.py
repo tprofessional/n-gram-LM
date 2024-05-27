e46e31c89509c4e1a310bef2d339d1eb38265b54
Trisha Moorkoth <tmoo2620@gmail.com>
2024-05-26 17:22:03 -0700

    added ui for testing

--- a2.py
+++ a2.py
@@ -154,7 +154,7 @@ def count_ngrams(text, n):
 
 
 # take the output of count_ngrams() to calculate probability for a sentence of tokens
-def probabilities(counts):
+def probabilities(counts, num_vocab = 0, smoothing = False, alpha = 1):
     # empty dict for probabilities of tokens
     probabilities = clt.defaultdict(clt.Counter)
     # iterate over each prefix and associated Counter object
@@ -164,7 +164,11 @@ def probabilities(counts):
         for token, count in counter.items():
             # calculate probability of token given the prefix and count of token
             # divided by total count and store value in dictionary
-            probabilities[prefix][token] = count / total_occurrences
+            if smoothing:
+                probabilities[prefix][token] = (count + alpha) / (total_occurrences + (num_vocab * alpha))
+                # print(probabilities[prefix][token])
+            else:
+                probabilities[prefix][token] = count / total_occurrences
 
     return probabilities
 
@@ -183,7 +187,7 @@ def sentence_probabilities(n, probabilities, text):
             prefix = tuple(ngram[:-1])
             # get the token
             token = ngram[-1]
-            # add the log probability of this token to the running sum -- MIGHT RUN INTO 0 PROB ERROR
+            # add the log probability of this token to the running sum
             sentence_log_prob += math.log2(probabilities[prefix][token]) if probabilities[prefix][token] > 0 else 0
         # store the sentence probability of the current sentence (stored as the first token of the sentence: prob)
         sentence_probs[s_idx] = sentence_log_prob
@@ -204,12 +208,47 @@ def insert_start(n, snt):
         snt.insert(0, '<START>')
     return snt
 
+'''
+Args:
+    lambdas = list of [l1, l2, l3]
+    uni_probs = unigram sentence probabilities
+    ...
+    data
+'''
+def linear_interpolation(lambdas, uni_probs, bi_probs, tri_probs, text):
+    # we want everything in groups of 3
+    n = 3
+    l1, l2, l3 = lambdas
+    sentence_log_prob = 0
+    sentence_probs = clt.defaultdict()
+    for s_idx, sentence in enumerate(text):
+        # for each token in the sentence
+        sentence_log_prob = 0
+        for tok_i in range(len(sentence) - (n-1)):
+            # get the ngram
+            ngram = sentence[tok_i : tok_i + n]
+            # get the prefix
+            tri_prefix = tuple(ngram[:-1])
+            bi_prefix = tuple(ngram[1:-1])
+            uni_prefix = tuple(ngram[2:-1])
+            # get the token
+            token = ngram[-1]
+            # add the log probability of this token to the running sum
+            sentence_log_prob += math.log2(l1 * uni_probs[uni_prefix][token] + l2 * bi_probs[bi_prefix][token] + l3 * tri_probs[tri_prefix][token])    
+        # store the sentence probability of the current sentence (stored as the first token of the sentence: prob)
+        sentence_probs[s_idx] = sentence_log_prob
+    return sentence_probs
+        
+
 
 if __name__ == "__main__":
     # Read the data
-    files = ['./A2-Data/1b_benchmark.train.tokens', './A2-Data/small.train.tokens', './A2-Data/smaller.train.tokens']
-    file = files[0]
+    # files = ['./A2-Data/1b_benchmark.train.tokens', './A2-Data/small.train.tokens', './A2-Data/smaller.train.tokens']
+    # file = files[0]
+    
+    file = './A2-Data/1b_benchmark.train.tokens'
     sentences = read(file)
+    print("Training on {}...".format(file))
 
     # preprocess the data
     tok_counts, all_counts = get_counts(sentences)
@@ -239,15 +278,16 @@ if __name__ == "__main__":
     '''
 
     # ---------- TESTING ------------
+    # prompt for test file
+    print("\n0: HDTV", "\t2: test data\n")
+    print("1: dev data\t3: train data\n")
+    FILE_IDX = int(input("Choose test data (enter 0 - 3): "))
     # read test data
-    # test_sentence = read('./A2-Data/1b_benchmark.dev.tokens')
-    test_sentence = read('./A2-Data/hdtv.test.tokens')
-    print("test sentence: ", test_sentence)
-
+    test_files = ['./A2-Data/hdtv.test.tokens', './A2-Data/1b_benchmark.dev.tokens', './A2-Data/1b_benchmark.test.tokens', './A2-Data/1b_benchmark.train.tokens']
+    test_sentence = read(test_files[FILE_IDX])
+    print('Test file: {}'.format(test_files[FILE_IDX]))
     # preprocess the data
     test_tok_counts, test_all_counts = get_counts(test_sentence)
-    print(test_tok_counts)
-    print(test_all_counts)
 
     # master tokenize test data
     uni_test_text = test_tokenize(vocab, test_sentence, 1)
@@ -259,22 +299,58 @@ if __name__ == "__main__":
     bi_snt_probs = sentence_probabilities(2, bigram_probs, bi_test_text)
     tri_snt_probs = sentence_probabilities(3, trigram_probs, tri_test_text)
 
-    print('\n-------- Sentence Probabilities --------\n')
+    '''print('\n-------- Sentence Probabilities --------\n')
     print('Unigram sentence probs:\n', uni_snt_probs, '\n')
     print('Bigram sentence probs:\n', bi_snt_probs, '\n')
     print('Trigram sentence probs:\n', tri_snt_probs, '\n')
+    '''
 
     # Calculate perplexity for a test sentence
     # print(test_tok_counts)
-    total_words = sum(test_tok_counts.values())
+    total_occs = sum(test_tok_counts.values())
 
-    print("test total words: ", total_words)
+    # print("test total words: ", total_occs)
 
     print('\n-------- Perplexities --------\n')
-    print('Unigram Perplexity: ', perplexity(uni_snt_probs, total_words))
-    print('Bigram Perplexity: ', perplexity(bi_snt_probs, total_words))
-    print('Trigram Perplexity: ', perplexity(tri_snt_probs, total_words))
+    print('Unigram Perplexity: ', perplexity(uni_snt_probs, total_occs))
+    print('Bigram Perplexity: ', perplexity(bi_snt_probs, total_occs))
+    print('Trigram Perplexity: ', perplexity(tri_snt_probs, total_occs))
+    
+    print('\n-------- LINEAR INTERPOLATION --------\n')
+    lambdas = [0.1, 0.3, 0.6]
 
+    lin = linear_interpolation(lambdas, unigram_probs, bigram_probs, trigram_probs, tri_test_text)
+    # print("linear interpolation: ", lin)
+    lin_perplexity = perplexity(lin, total_occs)
+    print('lambda 1 = ', lambdas[0], '\tlambda 2 = ', lambdas[1], '\tlambda 3 = ', lambdas[2])
+    print('\nPerplexity with linear interpolation: ', lin_perplexity)
 
-    if file == files[0]:
-        assert len(tok_counts) == 26602, "Did not identify 26,602 unique tokens"
+    print('\n-------- ADDITIVE SMOOTHING --------\n')
+    # get total train words
+    num_vocab = len(tok_counts)
+    # print("total_words: ", num_vocab)
+
+    alphas = [1, 0.05, 0.0001, 0.0000000001]
+
+    for a in alphas:
+        unigram_probs = probabilities(unigram_counts, num_vocab, smoothing = True, alpha = a)
+        bigram_probs = probabilities(bigram_counts, num_vocab, smoothing = True, alpha = a)
+        trigram_probs = probabilities(trigram_counts, num_vocab, smoothing = True, alpha = a)
+
+        # get sentence probabilities for the test data
+        uni_snt_probs = sentence_probabilities(1, unigram_probs, uni_test_text)
+        bi_snt_probs = sentence_probabilities(2, bigram_probs, bi_test_text)
+        tri_snt_probs = sentence_probabilities(3, trigram_probs, tri_test_text)
+
+        '''print('\n-------- Smoothed Sentence Probabilities --------\n')
+        print('Unigram sentence probs:\n', uni_snt_probs, '\n')
+        print('Bigram sentence probs:\n', bi_snt_probs, '\n')
+        print('Trigram sentence probs:\n', tri_snt_probs, '\n')
+        '''
+
+        print('\n--- Alpha = {} ---\n'.format(a))
+        # print('Alpha = ', a)
+        print('Unigram Perplexity: ', perplexity(uni_snt_probs, total_occs))
+        print('Bigram Perplexity: ', perplexity(bi_snt_probs, total_occs))
+        print('Trigram Perplexity: ', perplexity(tri_snt_probs, total_occs))
+    
